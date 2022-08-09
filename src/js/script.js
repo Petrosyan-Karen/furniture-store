f66cf7c05101ad0029f2119285a2553438417e9a
K555218 <kpn16@mail.ru>
2022-08-09 21:56:23 +0400

    fixed some buges, in home page we greated 2 blocks with swiper slider

--- src/js/script.js
+++ src/js/script.js
@@ -56,4 +56,27 @@ window.addEventListener("DOMContentLoaded", () => {
    
   
   } catch {}
+
+  try {
+    var swiper = new Swiper(".swiper-container", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+        loop: true,
+       
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      breakpoints: {
+         768: {
+          slidesPerView: 1,
+          spaceBetween: 10
+        },
+         1024: {
+          slidesPerView: 2,
+          spaceBetween: 20
+        },
+      }
+    });
+  } catch {}
 });
